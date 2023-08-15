2f002fe88e8935e34b58b3a802263478da941b35
Lucas <lucassoares2007@hotmail.com>
2023-08-15 05:33:15 -0300

    refactor: script definicao_title

--- scripts/script.py
+++ scripts/script.py
@@ -1,11 +1,15 @@
-def definicao_title(title: str, localidade="", sexo="") -> str:
+def definicao_title(
+    title: str,
+    is_localidade: bool,
+    sexo="",
+) -> str:
     if sexo == "M":
         title += " do sexo Masculino"
 
     if sexo == "F":
         title += " do sexo Feminino"
 
-    if localidade:
+    if is_localidade:
         title += f" por localidade"
 
     return f"{title}:\n"
